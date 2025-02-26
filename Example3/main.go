0e4fa21b8efa4e5dd135626b98d9ec8fd53f5ae8
Sony <geethasangamreddy0277@gmail.com>
2025-02-25 22:29:43 -0700

    this is my third commit

---
+++ Example3/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var student1 string   // in this condition we are declaring the variable type but not assigning the value
+	student1 = "John"     // in this condition we are assigning the value after declaring the varaiable type
+	fmt.Println(student1) // in this line the given student1 value will be printed ()
+
+}
