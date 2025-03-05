60c066f2da7f892b29b852640774b42ead91624a
Sony <geethasangamreddy0277@gmail.com>
2025-03-04 20:16:55 -0700

    deleting

--- Example15/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	var i, j = 10, 20 // assinging values to variable i,j
-
-	fmt.Print(i, j) // this funtion prints i and j value with space between as they are not strings.
-}
