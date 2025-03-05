630171ae65f9d684050638b387e94a01fb082904
Sony <geethasangamreddy0277@gmail.com>
2025-03-04 20:22:32 -0700

    delete

--- Example28/main.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	var x float64 = 1.7e+308                // declaring x variable with data type float64 and value 1.7e+308
-	fmt.Printf("Type: %T, value: %v", x, x) //this function printf() first formats its argument with formating verb given %T and then prints given x data type and formating verb given %v and then prints given x value.
-}
