a4c4116fa3d2fd66a7679c1867c5865367f80367
Sony <geethasangamreddy0277@gmail.com>
2025-03-05 22:01:21 -0700

    Palindrome_Number

--- Week1/Palindrome_Number/main.go
+++ Week1/Palindrome_Number/main.go
@@ -2,22 +2,28 @@ package main
 
 import "fmt"
 
-func palindrome(numb int)int {
+func palindrome(numb int) bool {
 
-	
-	
-	if palindrome == 0 {
-		fmt.Println("it is a Palindrome number")
-	} else {
-		fmt.Println("it is not a Palindrome number")
+	orginal := numb
+	reverse := 0
+	for numb > 0 {
+		reminder := numb % 10
+		reverse = reverse*10 + reminder
+		numb = numb / 10
+
 	}
+	return orginal == reverse
 
 }
-
 func main() {
 	var numb int
 	fmt.Println("Enter a number to check if the number is Palindrome:")
 	fmt.Scan(&numb)
 
-	palindrome(numb)
+	if palindrome(numb) {
+		fmt.Println("it is a Palindrome number")
+	} else {
+		fmt.Println("it is not a Palindrome number")
+	}
+
 }
